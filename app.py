8c3fc228770e1fd091ac8844fee0c9ba1ea772af
Shivam Sharma <54909615+shivamsharma00@users.noreply.github.com>
2023-08-22 17:47:00 -0700

    changes in app

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates/")
 
 model = pickle.load(open("regression_model.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
@@ -21,6 +21,16 @@ def predict_api():
     output = model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The house price is {}".format(output))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
